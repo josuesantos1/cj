5df9cc9b123c0c36af72b1353fec0fe865a6426a
josuesantos1 <josuecsantos2002@gmail.com>
2022-02-12 03:18:44 -0300

    CHORE: configuration file parse

---
+++ config.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/hcl/v2/hclsimple"
+)
+
+type Config struct {
+	Profile string `hcl:"profile"`
+	Region  string `hcl:"region"`
+	Path    string `hcl:"path,optional"`
+	Shell   string `hcl:"shell,optional"`
+}
+
+func Configure() Config {
+	var config Config
+	err := hclsimple.DecodeFile("cj.hcl", nil, &config)
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %s", err)
+	}
+	fmt.Printf("%#v\n", config)
+	return config
+}
