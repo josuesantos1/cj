de2c23ceaf5790c1a394db0b3032d8a853f48181
josuesantos1 <josuecsantos2002@gmail.com>
2022-02-15 00:36:12 -0300

    FEAT: create workspace

---
+++ workspace.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+)
+
+type Workspace struct {
+	Name       string    `json:"name"`
+	Ami        string    `json:"ami"`
+	Type       string    `json:"type"`
+	InstanceID string    `json:"instance_id"`
+	Ipv4       string    `json:"ipv4"`
+	CreatedAt  time.Time `json:"created_at"`
+	PrivateKey string    `json:"private_key"`
+}
+
+func CreateWorkspace(workspace Workspace) {
+	file, err := os.Create("cj.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	wJson, err := json.Marshal(workspace)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = file.WriteString(string(wJson))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
